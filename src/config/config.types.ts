262b47b03cd069dc4797607f11f080fcfd95eb45
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-25 00:07:07 +0300

    feat: integrate TypeORM and PostgreSQL for database connectivity

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -6,5 +6,10 @@ export interface ConfigType {
 }
 
 export const appConfigSchema = Joi.object({
-  APP_MESSAGE_PREFIX: Joi.string().default('Hello'),
+  APP_MESSAGE_PREFIX: Joi.string().default('Hello '),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5432),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
 });
