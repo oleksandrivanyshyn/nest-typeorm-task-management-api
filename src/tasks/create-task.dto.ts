37368241232c92d7885726273d102d9df2e7a045
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-24 20:14:19 +0300

    feat: integrate class-validator and class-transformer for DTO validation, add global validation pipe

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,7 +1,14 @@
 import { TaskStatus } from './task.model';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+  @IsNotEmpty()
+  @IsString()
   description: string;
+  @IsNotEmpty()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }
