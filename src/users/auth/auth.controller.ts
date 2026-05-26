e7b48e85ed8f6b29b60665c8f346f1e9284956f2
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-26 11:01:10 +0300

    feat: implement authentication module with registration and login endpoints

---
+++ src/users/auth/auth.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  NotFoundException,
+  Post,
+  Request,
+  SerializeOptions,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from '../create-user.dto';
+import { AuthService } from './auth.service';
+import { User } from '../user.entity';
+import { LoginDto } from '../login.dto';
+import { LoginResponse } from '../login.response';
+import type { AuthRequest } from '../auth.request';
+import { UserService } from '../user/user.service';
+import { AuthGuard } from '../auth.guard';
+
+@Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+@SerializeOptions({ strategy: 'excludeAll' })
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto): Promise<User> {
+    const user = await this.authService.register(createUserDto);
+    return user;
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
+    const accessToken = await this.authService.login(
+      loginDto.email,
+      loginDto.password,
+    );
+
+    return new LoginResponse({ accessToken });
+  }
+
+  @Get('/profile')
+  @UseGuards(AuthGuard)
+  async profile(@Request() request: AuthRequest): Promise<User> {
+    const user = await this.userService.findOne(request.user.sub);
+
+    if (user) {
+      return user;
+    }
+
+    throw new NotFoundException();
+  }
+}
